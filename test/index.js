925820e907e4c769b66c6ea9fea585dce3cbbf57
Christopher Jeffrey <chjjeffrey@gmail.com>
2011-11-06 12:11:53 -0600

    remove streaming qs and json parsers, remove legacy code

--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ try {
   fs.mkdirSync(files, 0755);
 }
 
-parted.root = files;
+parted.multipart.root = files;
 
 // create a mock request
 var request = function(size, file) {
@@ -49,7 +49,7 @@ var request = function(size, file) {
   };
 };
 
-var handle = parted.middleware();
+var handle = parted.multipart.middleware();
 
 var expect_text = {
   chrome: 'world...oh look the end'
@@ -118,102 +118,13 @@ var multiple = function(file, func) {
 };
 
 var main = function(argv) {
-  if (!argv.length || ~argv.indexOf('--multipart')) {
-    multiple('chrome', function() {
-      multiple('opera', function() {
-        multiple('firefox', function() {
-          console.log('DONE');
-          if (!argv.length) json(), encoded();
-        });
+  multiple('chrome', function() {
+    multiple('opera', function() {
+      multiple('firefox', function() {
+        console.log('DONE');
       });
     });
-  }
-
-  if (~argv.indexOf('--json')) {
-    json();
-  }
-
-  if (~argv.indexOf('--encoded')) {
-    encoded();
-  }
-};
-
-var json = function() {
-  var assert = require('assert');
-  var TEST_JSON = '{"a":{"b":100,"c":[2,3]},"d":["e"],"f":true}';
-  var Parser = require('../lib/json');
-
-  var parser = new Parser();
-
-  parser.on('end', function(obj) {
-    console.log(obj);
-    assert.deepEqual(obj, JSON.parse(TEST_JSON), 'Not deep equal.');
-    assert.equal(JSON.stringify(obj), TEST_JSON, 'Not equal.');
-    console.log('Completed.');
   });
-
-  var emit = parser.emit;
-  parser.emit = function(type, val) {
-    console.log(type, val);
-    return emit.apply(this, arguments);
-  };
-
-  parser.write(TEST_JSON.slice(0, 25));
-  parser.write(TEST_JSON.slice(25));
-  parser.end();
-
-  /*
-  (function emit(a, b) {
-    parser.emit(a, b);
-    return emit;
-  })
-  ('object start')
-  ('object key', 'hello')
-  ('object start')
-  ('object key', 'world')
-  ('number', 100)
-  ('object key', 'test')
-  ('array start')
-  ('number', 2)
-  ('number', 3)
-  ('array end')
-  ('object end')
-  ('object key', 'hi')
-  ('array start')
-  ('string', 'an array!')
-  ('array end')
-  ('object end')
-  ('end', parser.data);
-  */
-};
-
-var encoded = function() {
-  var assert = require('assert')
-    , qs = require('querystring')
-    , obj = {}
-    , Parser = require('../lib/encoded');
-
-  var TEST_ENCODED = 'a=1&b=2&c=3&d=hello%20world&e=hi%20world&f=testing';
-
-  var parser = new Parser();
-
-  parser.on('value', function(key, value) {
-    console.log(key, value);
-    obj[key] = value;
-  });
-
-  parser.on('end', function() {
-    console.log(obj);
-    assert.deepEqual(obj, qs.parse(TEST_ENCODED), 'Not deep equal.'
-      + require('util').inspect(qs.parse(TEST_ENCODED)));
-    assert.equal(qs.stringify(obj), TEST_ENCODED, 'Not equal.'
-      + qs.stringify(obj));
-    console.log('Completed.');
-  });
-
-  parser.write(new Buffer(TEST_ENCODED.slice(0, 25), 'utf8'));
-  parser.write(new Buffer(TEST_ENCODED.slice(25), 'utf8'));
-  parser.end();
 };
 
 main(process.argv.slice(2));
